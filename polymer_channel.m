fc32458653928a9d2343d035ecfb745e62f4486e
hjzuo <32312577+hjzuo@users.noreply.github.com>
2018-05-26 15:23:16 -0400

    Unit changes
    Unit changes
    Plot scale changes

--- polymer_channel.m
+++ polymer_channel.m
@@ -1,5 +1,4 @@
-% This example computes all of the field components of a
-% silicon-on-insulator nanowire waveguide.
+% This example computes all of the field components of a polymer channel waveguide revised 5/26/2018.
 
 ncore=1.542;
 nclad=1.523;
@@ -8,18 +7,18 @@ n1 = nclad;          %  lower cladding
 n2 = ncore;          %  core
 n3 = nclad;          % upper cladding
 
-h1 = 8000;           % lower cladding (nm)
-h2 = 2300;           % silicon core (nm)
-h3 = 5000;           % upper cladding (nm)
+h1 = 8.000;           % lower cladding (um)
+h2 = 2.300;           % silicon core (um)
+h3 = 5.000;           % upper cladding (um)
 
-dx = 20;           % grid size (x)
+dx = 0.020;           % grid size (x)
 dy = dx;            % grid size (y)
 
-lambda = 850;      % wavelength (nm)
+lambda = 0.850;      % wavelength (nm)
 nmodes = 2;         % number of modes to compute
 
-w = 2300;            % waveguide full-width (nm)
-side = 5500;         % space on side of waveguide (nm)
+w = 2.300;            % waveguide full-width (um)
+side = 5.500;         % space on side of waveguide (um)
 
 fprintf (1,'generating index mesh...\n');
 
@@ -33,15 +32,23 @@ fprintf (1,'generating index mesh...\n');
 
 fprintf(1,'neff = %7.5f\n',neff_all);
 
+
+%% image mode
+
+
 i = 1;
 Hx=Hx_all(:,:,i);Hy=Hy_all(:,:,i);neff=neff_all(i);
 [Hz,Ex,Ey,Ez] = postprocess (lambda, neff, Hx, Hy, dx, dy, eps, '000A');
 
+
 ii = i;
 colormap(jet(256));
-hn = abs(interp2(y,x,Hy,h1+h2/2,0));
-en = abs(interp2(yc,xc,Ex,h1+h2/2,min(dx)/2));
-
+hn_y = abs(interp2(y,x,Hy,h1+h2/2,0));
+hn_x = abs(interp2(y,x,Hx,h1+h2/2,0));
+hn = max(hn_x,hn_y);
+en_x = abs(interp2(yc,xc,Ex,h1+h2/2,min(dx)/2));
+en_y = abs(interp2(yc,xc,Ey,h1+h2/2,min(dx)/2));
+en = max(en_x,en_y);
 
 subplot(231);
 imagemode(x,y,Hx/hn);
